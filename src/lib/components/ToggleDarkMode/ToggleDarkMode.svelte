44c52139c804c2917f66319c1064e395bef7a23c
RAVENA CAMPOS <ravenascampos@gmail.com>
2024-09-06 16:33:49 -0300

    ✨ feat(toggle-dark-mode): add ToggleDarkMode component

---
+++ src/lib/components/ToggleDarkMode/ToggleDarkMode.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import LightModeIcon from '@/lib/icons/LightModeIcon.svelte';
+	import DarkModeIcon from '@/lib/icons/DarkModeIcon.svelte';
+	let isDarkMode = false;
+
+	const toggleDarkMode = () => {
+		isDarkMode = !isDarkMode;
+		if (isDarkMode) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	};
+</script>
+
+<button
+	on:click={toggleDarkMode}
+	class="text-h3 p-2 bg-light-bg dark:bg-dark-bg text-primary dark:text-secondary rounded-md"
+>
+	{#if isDarkMode}
+		<DarkModeIcon />
+	{:else}
+		<LightModeIcon />
+	{/if}
+</button>
