a0a3b00f7b1ff5e76e7c753381f33d44fb6701ad
RAVENA CAMPOS <ravenascampos@gmail.com>
2024-09-09 05:46:45 -0300

    ✨ feat (toggle-dark-mode): update ToggleDarkMode component

--- src/lib/components/ToggleDarkMode/ToggleDarkMode.svelte
+++ src/lib/components/ToggleDarkMode/ToggleDarkMode.svelte
@@ -1,24 +1,29 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { isDarkMode } from '@/stores/darkModeStore';
 	import LightModeIcon from '@/lib/icons/LightModeIcon.svelte';
 	import DarkModeIcon from '@/lib/icons/DarkModeIcon.svelte';
-	let isDarkMode = false;
 
-	const toggleDarkMode = () => {
-		isDarkMode = !isDarkMode;
-		if (isDarkMode) {
-			document.documentElement.classList.add('dark');
-		} else {
-			document.documentElement.classList.remove('dark');
-		}
-	};
+	function toggleDarkMode() {
+		isDarkMode.update((value) => {
+			const newValue = !value;
+			if (newValue) {
+				document.documentElement.classList.add('dark');
+			} else {
+				document.documentElement.classList.remove('dark');
+			}
+			return newValue;
+		});
+	}
+
+	$isDarkMode;
 </script>
 
 <button
 	on:click={toggleDarkMode}
 	class="text-h3 p-2 bg-light-bg dark:bg-dark-bg text-primary dark:text-secondary rounded-md"
 >
-	{#if isDarkMode}
+	{#if $isDarkMode}
 		<DarkModeIcon />
 	{:else}
 		<LightModeIcon />
