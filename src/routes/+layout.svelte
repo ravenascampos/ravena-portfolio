4a75ecd823228131ab5faf0ce2787777b8dd9825
RAVENA CAMPOS <ravenascampos@gmail.com>
2024-09-09 05:47:09 -0300

    ✨ feat (layout): add layout component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+	import AppHeader from './../lib/components/AppHeader/AppHeader.svelte';
+	import Footer from '@/lib/components/Footer/Footer.svelte';
+</script>
+
+<AppHeader />
+<div class="container">
+	<slot></slot>
+</div>
+<Footer />
